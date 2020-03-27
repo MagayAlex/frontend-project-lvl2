7ee1ca48a6ca6cc39cdb281ea085e1c1df2f6498
Alex Magay <magayalex@gmail.com>
2020-03-27 20:16:30 +0300

    step 6

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,20 +2,13 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src/index.js';
 
-test('test #1', () => {
-  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-  const file1 = getFixturePath('before.json');
-  const file2 = getFixturePath('after.json');
-  // const pathResult = getFixturePath('result.txt');
-  // const resultData = fs.readFileSync(resultPath);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const arr = [['before.json', 'after.json', 'result.txt'], ['before.yaml', 'after.yaml', 'result.txt'], ['1.yaml', '2.yaml', '3.txt'], ['before.ini', 'after.ini', 'result.txt']];
 
-
-  const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-  const temp = readFile('result.txt');
-  // console.log('temp= ', temp);
-  const result = temp;// JSON.parse(temp);
-  console.log('result = ', result);
-  // const file2 = readFile('after.json');
-  expect(genDiff(file1, file2)).toEqual(result);
+test.each(arr)('test %#', (a, b, result) => {
+  const file1 = getFixturePath(a);
+  const file2 = getFixturePath(b);
+  const expected = readFile(result);
+  expect(genDiff(file1, file2)).toEqual(expected);
 });
-// ' \n   host: hexlet.io \n + timeout: 20 \n - timeout: 50 \n - proxy: 123.234.53.22 \n - follow: false \n + verbose: true'
--- src/index.js
+++ src/index.js
@@ -1,44 +1,38 @@
 import lodash from 'lodash';
-import fs from 'fs';
-import path from 'path';
+import parser from './parsers.js';
 
 const { has } = lodash;
 
 const genDiff = (pathToFile1, pathToFile2) => {
-  const file1Path = path.resolve(process.cwd(), pathToFile1);
-  const file2Path = path.resolve(process.cwd(), pathToFile2);
-  const data1 = fs.readFileSync(file1Path);
-  const data2 = fs.readFileSync(file2Path);
-  if (path.extname(pathToFile1) !== '.json' || path.extname(pathToFile2) !== '.json') {
-    return console.log('Error: this type of files doesnt support');
-  }
-  const parsedJSON1 = JSON.parse(data1);
-  const parsedJSON2 = JSON.parse(data2);
+  const parsedFile1 = parser(pathToFile1);
+  const parsedFile2 = parser(pathToFile2);
   const reducer = (acc, n) => {
     const [key, value] = n;
-    if (has(parsedJSON2, key) && parsedJSON2[key] !== value) {
-      acc = `${acc} \n + ${key}: ${parsedJSON2[key]} \n - ${key}: ${value}`;
+    const temp = [];
+    if (has(parsedFile2, key) && parsedFile2[key] !== value) {
+      temp.push(`  + ${key}: ${parsedFile2[key]}\n  - ${key}: ${value}`);
     }
-    if (has(parsedJSON2, key) && parsedJSON2[key] === value) {
-      acc = `${acc} \n   ${key}: ${value}`;
+    if (has(parsedFile2, key) && parsedFile2[key] === value) {
+      temp.push(`    ${key}: ${value}`);
     }
-    if (!has(parsedJSON2, key)) {
-      acc = `${acc} \n - ${key}: ${value}`;
+    if (!has(parsedFile2, key)) {
+      temp.push(`  - ${key}: ${value}`);
     }
-    return acc;
+    return [...acc, temp];
+    // return acc;
   };
-  const jsonArray = Object.entries(parsedJSON1);
-  const json2keys = Object.keys(parsedJSON2);
-  const json1keys = Object.keys(parsedJSON1);
+  const jsonArray = Object.entries(parsedFile1);
+  const json2keys = Object.keys(parsedFile2);
+  const json1keys = Object.keys(parsedFile1);
   const temp = jsonArray.reduce(reducer, '');
   const result = json2keys.reduce((acc, key) => {
     if (!json1keys.includes(key)) {
-      acc = `${acc} \n + ${key}: ${parsedJSON2[key]}`;
+      return [...acc, `  + ${key}: ${parsedFile2[key]}`];
     }
     return acc;
   }, temp);
-  console.log('result= ', result);
-  return result;
+  const x = result.join('\n');
+  return `{\n${x}\n}`;
 };
 
 export default genDiff;
