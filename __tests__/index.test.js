2e48577946cfa5c8218c5becd9f6108d0ef069b9
Alex Magay <magayalex@gmail.com>
2020-03-31 23:15:05 +0300

    step 7

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,7 +4,8 @@ import genDiff from '../src/index.js';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-const arr = [['before.json', 'after.json', 'result.txt'], ['before.yaml', 'after.yaml', 'result.txt'], ['1.yaml', '2.yaml', '3.txt'], ['before.ini', 'after.ini', 'result.txt']];
+const arr = [['beforeTree.json', 'afterTree.json', 'resultTree.txt'], ['before.json', 'after.json', 'result.txt'], ['before.yaml', 'after.yaml', 'result.txt'], ['beforeTree.yaml', 'afterTree.yaml', 'resultTree.txt'],
+  ['before.ini', 'after.ini', 'result.txt'], ['beforeTree.ini', 'afterTree.ini', 'resultTree.txt']];
 
 test.each(arr)('test %#', (a, b, result) => {
   const file1 = getFixturePath(a);
--- src/index.js
+++ src/index.js
@@ -1,38 +1,65 @@
 import lodash from 'lodash';
 import parser from './parsers.js';
 
-const { has } = lodash;
+const {
+  has, uniq, isEqual,
+} = lodash;
 
 const genDiff = (pathToFile1, pathToFile2) => {
   const parsedFile1 = parser(pathToFile1);
   const parsedFile2 = parser(pathToFile2);
-  const reducer = (acc, n) => {
-    const [key, value] = n;
-    const temp = [];
-    if (has(parsedFile2, key) && parsedFile2[key] !== value) {
-      temp.push(`  + ${key}: ${parsedFile2[key]}\n  - ${key}: ${value}`);
-    }
-    if (has(parsedFile2, key) && parsedFile2[key] === value) {
-      temp.push(`    ${key}: ${value}`);
-    }
-    if (!has(parsedFile2, key)) {
-      temp.push(`  - ${key}: ${value}`);
-    }
-    return [...acc, temp];
-    // return acc;
-  };
-  const jsonArray = Object.entries(parsedFile1);
-  const json2keys = Object.keys(parsedFile2);
-  const json1keys = Object.keys(parsedFile1);
-  const temp = jsonArray.reduce(reducer, '');
-  const result = json2keys.reduce((acc, key) => {
-    if (!json1keys.includes(key)) {
-      return [...acc, `  + ${key}: ${parsedFile2[key]}`];
-    }
-    return acc;
-  }, temp);
-  const x = result.join('\n');
-  return `{\n${x}\n}`;
-};
 
+  const buildObj = (key, value, atributes, children = []) => ({
+    key, value, atributes, children,
+  });
+  const stringify = (x) => Object.entries(x).map(([key, value]) => `${key}: ${value}`).join('\n');
+  const objToText = (obj, counter) => {
+    const tab = '    ';
+    const atr = (x) => {
+      if (x === 'unmodified') {
+        return tab;
+      }
+      if (x === 'added') {
+        return '  + ';
+      }
+      return '  - ';
+    };
+
+    if (typeof obj.value === 'object') {
+      return `\n${tab.repeat(counter)}${atr(obj.atributes)}${obj.key}: {\n${tab.repeat(counter + 2)}${stringify(obj.value)}\n${tab.repeat(counter + 1)}}`;
+    }
+    if (isEqual(obj.children, [])) {
+      return `\n${tab.repeat(counter)}${atr(obj.atributes)}${obj.key}: ${obj.value}`;
+    }
+
+    return `\n${tab.repeat(counter)}${atr(obj.atributes)}${obj.key}: {${obj.children.map((node) => objToText(node, counter + 1)).join('')}\n${tab.repeat(counter + 1)}}`;
+  };
+  const iter = (data1, data2) => {
+    const uniqueKeys = uniq([...Object.keys(data1), ...Object.keys(data2)]).sort();
+    const result = uniqueKeys.reduce((acc, key) => {
+      if (typeof data1[key] === 'object' && typeof data2[key] === 'object') {
+        return [...acc, buildObj(key, '', 'unmodified', iter(data1[key], data2[key]))];
+      }
+      if (data1[key] === data2[key]) {
+        return [...acc, buildObj(key, data1[key], 'unmodified', [])];
+      }
+      if (has(data1, key) && !has(data2, key)) {
+        return [...acc, buildObj(key, data1[key], 'deleted', [])];
+      }
+      if (!has(data1, key) && has(data2, key)) {
+        return [...acc, buildObj(key, data2[key], 'added', [])];
+      }
+      const x = [...acc, buildObj(key, data1[key], 'deleted', [])];
+      return [...x, buildObj(key, data2[key], 'added', [])];
+    }, []);
+    return result;
+  };
+  const astTree = iter(parsedFile1, parsedFile2);
+
+  const render = (tree) => tree.reduce((acc, node) => `${acc}${objToText(node, 0)}`,
+    '');
+  const x = render(astTree);
+  // console.log('x= \n', astTree[0].children);
+  return `{${x}\n}`;
+};
 export default genDiff;
