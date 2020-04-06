82990861620f91d0f79321a9e2690252de5d4b02
Alex Magay <magayalex@gmail.com>
2020-04-06 16:57:20 +0300

    fix codeclimate recommendation

--- src/parser.js
+++ src/parser.js
@@ -7,7 +7,6 @@ import lodash from 'lodash';
 const {
   has, uniq,
 } = lodash;
-
 const parser = (file) => {
   const filePath = path.resolve(process.cwd(), file);
   const data = fs.readFileSync(filePath, 'utf8');
@@ -24,7 +23,7 @@ const parser = (file) => {
 };
 const getAst = (data1, data2) => {
   const uniqueKeys = uniq([...Object.keys(data1), ...Object.keys(data2)]).sort();
-  const result = uniqueKeys.reduce((acc, key) => {
+  return uniqueKeys.reduce((acc, key) => {
     if (typeof data1[key] === 'object' && typeof data2[key] === 'object') {
       return [...acc, { atr: 'unchanged', key, children: getAst(data1[key], data2[key]) }];
     }
@@ -32,15 +31,14 @@ const getAst = (data1, data2) => {
       return [...acc, { atr: 'unchanged', key, value: data1[key] }];
     }
     if (has(data1, key) && !has(data2, key)) {
-      return [...acc, { atr: 'deleted', key, value: data1[key] }]; // buildObj(key, data1[key], 'deleted', [])];
+      return [...acc, { atr: 'deleted', key, value: data1[key] }];
     }
     if (!has(data1, key) && has(data2, key)) {
-      return [...acc, { atr: 'added', key, value: data2[key] }]; // buildObj(key, data2[key], 'added', [])];
+      return [...acc, { atr: 'added', key, value: data2[key] }];
     }
     return [...acc, {
       atr: 'changed', key, newValue: data2[key], oldValue: data1[key],
     }];
   }, []);
-  return result;
 };
 export { getAst, parser };
