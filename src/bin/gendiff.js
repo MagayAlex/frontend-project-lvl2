9a336eb754cec701b7b4d74d97ab8476b9626a61
Alex Magay <magayalex@gmail.com>
2020-04-06 21:28:49 +0300

    delete unnessesary =

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
-import commander from 'commander';
+import program from 'commander';
 import genDiff from '../index.js';
 
-
-const program = commander;
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
