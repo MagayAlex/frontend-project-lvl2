d5533e0badf285c560e2474769d147297302300c
Alex Magay <magayalex@gmail.com>
2020-04-06 16:53:39 +0300

    fix codeclimate recommendation

--- src/formatters/treeView.js
+++ src/formatters/treeView.js
@@ -1,35 +1,30 @@
-const treeView = (data) => {
-  const render = (obj, counter = 0) => {
-    const tab = '    ';
-    const stringify = (x) => {
-      const temp = `{\n${tab.repeat(counter + 2)}${Object.entries(x).map(([key, value]) => `${key}: ${value}`).join()}\n${tab.repeat(counter + 1)}}`;
-      return typeof x === 'object' ? temp : `${x}`;
-      /* if (typeof x === 'object') {
-        return `{\n${tab.repeat(counter + 2)}${Object.entries(x).map(([key, value]) => `${key}: ${value}`).join()}\n${tab.repeat(counter + 1)}}`;
-      }
-      return `${x}`; */
-    };
-    const atr = (x) => {
-      if (x === 'unchanged' || x === 'changed') {
-        return tab;
-      }
-      if (x === 'added') {
-        return '  + ';
-      }
-      if (x === 'deleted') return '  - ';
-      return 'ne naideno';
-    };
+const tab = '    ';
+const atr = (x) => {
+  if (x === 'unchanged' || x === 'changed') {
+    return tab;
+  }
+  if (x === 'added') {
+    return '  + ';
+  }
+  if (x === 'deleted') return '  - ';
+  return 'ne naideno';
+};
 
-    if (obj.atr === 'changed') {
-      return `\n${tab.repeat(counter)}${atr('deleted')}${obj.key}: ${stringify(obj.oldValue)}\n${tab.repeat(counter)}${atr('added')}${obj.key}: ${stringify(obj.newValue)}`;
-    }
-    if (obj.children) {
-      return `\n${tab.repeat(counter)}${atr(obj.atr)}${obj.key}: {${obj.children.map((node) => render(node, counter + 1)).join('')}\n${tab.repeat(counter + 1)}}`;
-    }
-    return `\n${tab.repeat(counter)}${atr(obj.atr)}${obj.key}: ${stringify(obj.value)}`;
+const render = (obj, counter = 0) => {
+  const stringify = (x) => {
+    const temp = `{\n${tab.repeat(counter + 2)}${Object.entries(x).map(([key, value]) => `${key}: ${value}`).join()}\n${tab.repeat(counter + 1)}}`;
+    return typeof x === 'object' ? temp : `${x}`;
   };
-  const rendered = (tree) => tree.reduce((acc, node) => `${acc}${render(node, 0)}`, '');
-  const x = rendered(data);
+  if (obj.atr === 'changed') {
+    return `\n${tab.repeat(counter)}${atr('deleted')}${obj.key}: ${stringify(obj.oldValue)}\n${tab.repeat(counter)}${atr('added')}${obj.key}: ${stringify(obj.newValue)}`;
+  }
+  if (obj.children) {
+    return `\n${tab.repeat(counter)}${atr(obj.atr)}${obj.key}: {${obj.children.map((node) => render(node, counter + 1)).join('')}\n${tab.repeat(counter + 1)}}`;
+  }
+  return `\n${tab.repeat(counter)}${atr(obj.atr)}${obj.key}: ${stringify(obj.value)}`;
+};
+const treeView = (data) => {
+  const x = data.reduce((acc, node) => `${acc}${render(node, 0)}`, '');
   return `{${x}\n}`;
 };
 
