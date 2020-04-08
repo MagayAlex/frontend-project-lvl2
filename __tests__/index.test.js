1a9c21eb1c353ce916b16990d2ce52f2eff206aa
Alex Magay <magayalex@gmail.com>
2020-04-08 22:57:54 +0300

    add new tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,11 +4,11 @@ import genDiff from '../src/index.js';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-const arr = [['beforeTree.json', 'afterTree.json', 'tree', 'resultTree.txt'], ['before.json', 'after.json', 'tree', 'result.txt'],
-  ['before.yaml', 'after.yaml', 'tree', 'result.txt'], ['beforeTree.yaml', 'afterTree.yaml', 'tree', 'resultTree.txt'],
-  ['before.ini', 'after.ini', 'tree', 'result.txt'], ['beforeTree.ini', 'afterTree.ini', 'tree', 'resultTree.txt'],
-  ['beforeTree.json', 'afterTree.json', 'plain', 'resultPlain.txt'], ['before.json', 'after.json', 'plain', 'resultPlainSimple.txt'],
-  ['beforeTree.json', 'afterTree.json', 'json', 'resultJson.txt']];
+const arr = [['beforeTree.json', 'afterTree.json', 'tree', 'resultTree.txt'], ['beforeTree.yaml', 'afterTree.yaml', 'tree', 'resultTree.txt'],
+  ['beforeTree.ini', 'afterTree.ini', 'tree', 'resultTree.txt'], ['beforeTree.json', 'afterTree.json', 'plain', 'resultPlain.txt'],
+  ['beforeTree.yaml', 'afterTree.yaml', 'plain', 'resultPlain.txt'], ['beforeTree.ini', 'afterTree.ini', 'plain', 'resultPlain.txt'],
+  ['beforeTree.json', 'afterTree.json', 'json', 'resultJson.txt'], ['beforeTree.yaml', 'afterTree.yaml', 'tree', 'resultTree.txt'],
+  ['beforeTree.ini', 'afterTree.ini', 'json', 'resultJson.txt']];
 
 test.each(arr)('test %#', (a, b, type, result) => {
   const file1 = getFixturePath(a);
