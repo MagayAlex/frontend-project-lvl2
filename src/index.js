1faf5ba816f4669d1a6556f00eb1c11df9466464
Alex Magay <magayalex@gmail.com>
2020-04-13 22:39:04 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import { getAst, parser } from './parser.js';
-import formatters from './formatters';
+import fs from 'fs';
+import path from 'path';
+import parse from './parse.js';
+import buildAst from './buildAst.js';
+import format from './formatters';
 
+const data = (pathToFile) => fs.readFileSync(path.resolve(process.cwd(), pathToFile), 'utf8');
 const genDiff = (pathToFile1, pathToFile2, type = 'json') => {
-  const parsedFile1 = parser(pathToFile1);
-  const parsedFile2 = parser(pathToFile2);
-  const astTree = getAst(parsedFile1, parsedFile2);
-  return formatters(astTree, type);
+  const parsedFile1 = parse(data(pathToFile1), path.extname(pathToFile1));
+  const parsedFile2 = parse(data(pathToFile2), path.extname(pathToFile2));
+  const astTree = buildAst(parsedFile1, parsedFile2);
+  return format(astTree, type);
 };
 export default genDiff;
