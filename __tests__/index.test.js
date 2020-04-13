8b0a252a45f4b9b83ef183957792977a717fe9cd
Alex Magay <magayalex@gmail.com>
2020-04-13 22:39:36 +0300

    rename

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,15 +4,15 @@ import genDiff from '../src/index.js';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-const arr = [['beforeTree.json', 'afterTree.json', 'tree', 'resultTree.txt'], ['beforeTree.yaml', 'afterTree.yaml', 'tree', 'resultTree.txt'],
-  ['beforeTree.ini', 'afterTree.ini', 'tree', 'resultTree.txt'], ['beforeTree.json', 'afterTree.json', 'plain', 'resultPlain.txt'],
-  ['beforeTree.yaml', 'afterTree.yaml', 'plain', 'resultPlain.txt'], ['beforeTree.ini', 'afterTree.ini', 'plain', 'resultPlain.txt'],
-  ['beforeTree.json', 'afterTree.json', 'json', 'resultJson.txt'], ['beforeTree.yaml', 'afterTree.yaml', 'tree', 'resultTree.txt'],
-  ['beforeTree.ini', 'afterTree.ini', 'json', 'resultJson.txt']];
-
-test.each(arr)('test %#', (a, b, type, result) => {
-  const file1 = getFixturePath(a);
-  const file2 = getFixturePath(b);
-  const expected = readFile(result);
-  expect(genDiff(file1, file2, type)).toEqual(expected);
+const arr = [['tree', 'resultTree.txt'], ['plain', 'resultPlain.txt'], ['json', 'resultJson.txt']];
+const arrOfData = [['beforeTree.json', 'afterTree.json'], ['beforeTree.ini', 'afterTree.ini'], ['beforeTree.yaml', 'afterTree.yaml']];
+test.each(arr)('test %#', (type, result) => {
+  console.log(`test ${type} files`);
+  arrOfData.forEach((element) => {
+    const [before, after] = element;
+    const file1 = getFixturePath(before);
+    const file2 = getFixturePath(after);
+    const expected = readFile(result);
+    expect(genDiff(file1, file2, type)).toEqual(expected);
+  });
 });
