329a4e36a2054a125f3c5e2232c8b999c3c8a40b
Alex Magay <magayalex@gmail.com>
2020-04-14 11:40:18 +0300

    add  number or string check func

--- src/format/buildJsonView.js
+++ src/format/buildJsonView.js
@@ -1,1 +1,5 @@
-export default (data) => JSON.stringify(data);
+const isNumber = (n) => !isNaN(parseFloat(n)) && !isNaN(n - 0) ? parseInt(n) : n;
+
+export default (data) => {
+	return JSON.stringify(data, (key, value) => isNumber(value));
+};
