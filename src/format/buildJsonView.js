be45e71bdf54d4e3e658e2ca81a67dbed06a54a0
Alex Magay <magayalex@gmail.com>
2020-04-16 23:49:09 +0300

    delete unused function isNumber

--- src/format/buildJsonView.js
+++ src/format/buildJsonView.js
@@ -1,7 +1,1 @@
-const isNumber = (n) => {
-  if (!Number.isNaN(parseFloat(n)) && !Number.isNaN(n - 0)) {
-    return parseInt(n, 10);
-  }
-  return n;
-};
-export default (data) => JSON.stringify(data, (key, value) => isNumber(value));
+export default (data) => JSON.stringify(data);// , (key, value) => isNumber(value));
