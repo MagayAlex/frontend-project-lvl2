4033615016f6608e123c66b2fb4b929609a6c931
Alex Magay <magayalex@gmail.com>
2020-04-16 19:53:56 +0300

    add print to screen

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,5 +7,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format', 'json')
   .arguments('<file1>, <file2>')
-  .action((file1, file2) => genDiff(file1, file2, program.format))
+  .action((file1, file2) => {
+    console.log(genDiff(file1, file2, program.format));
+  })
   .parse(process.argv);
