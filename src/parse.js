f2fee53dd8116a5a0d5da1957d8dd9cbab565988
Alex Magay <magayalex@gmail.com>
2020-04-16 23:50:20 +0300

    fix ini-parser bug

--- src/parse.js
+++ src/parse.js
@@ -1,6 +1,22 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
 
+const isNumber = (n) => {
+  if (!Number.isNaN(parseFloat(n)) && !Number.isNaN(n - 0)) {
+    return parseInt(n, 10);
+  }
+  return n;
+};
+const iniFix = (obj) => {
+  const entries = Object.entries(obj);
+  return entries.reduce((acc, [key, value]) => {
+    if (value instanceof Object) {
+      return { ...acc, [key]: iniFix(value) };
+    }
+    return { ...acc, [key]: isNumber(value) };
+  }, {});
+};
+
 export default (data, extName) => {
   switch (extName) {
     case '.json':
@@ -8,7 +24,9 @@ export default (data, extName) => {
     case '.yaml':
       return yaml.load(data);
     case '.ini':
-      return ini.parse(data);
+      console.log('ini', iniFix(ini.parse(data)));
+      console.log('ini Before', ini.parse(data));
+      return iniFix(ini.parse(data));
     default:
       throw new Error(`Unknown file type: '${extName}'!`);
   }
