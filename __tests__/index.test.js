f77c7bb2ce6a79c83a9cb9e35f8a61414fa20ecd
Alex Magay <magayalex@gmail.com>
2020-04-16 19:42:31 +0300

    change test structure

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,16 +4,11 @@ import genDiff from '../src/index.js';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-const arr = [['tree', 'resultTree.txt'], ['plain', 'resultPlain.txt'], ['json', 'resultJson.txt']];
-
-const arrOfExtentions = ['.json', '.yaml', '.ini'];
-test.each(arr)('test %#', (type, result) => {
-  arrOfExtentions.forEach((etxtention) => {
-    const before = `beforeTree${etxtention}`;
-    const after = `afterTree${etxtention}`;
-    const file1 = getFixturePath(before);
-    const file2 = getFixturePath(after);
-    const expected = readFile(result);
-    expect(genDiff(file1, file2, type)).toEqual(expected);
-  });
+const arr = ['json', 'yaml', 'ini'];
+test.each(arr)('test %#', (etxtention) => {
+  const fileBefore = getFixturePath(`beforeTree.${etxtention}`);
+  const fileAfter = getFixturePath(`afterTree.${etxtention}`);
+  expect(genDiff(fileBefore, fileAfter, 'tree')).toEqual(readFile('resultTree.txt'));
+  expect(genDiff(fileBefore, fileAfter, 'plain')).toEqual(readFile('resultPlain.txt'));
+  expect(genDiff(fileBefore, fileAfter, 'json')).toEqual(readFile('resultJson.txt'));
 });
