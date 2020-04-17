9747b1f3152b510d0c318181746551af77d6f872
Alex Magay <magayalex@gmail.com>
2020-04-17 21:28:48 +0300

    delete console.log

--- src/parse.js
+++ src/parse.js
@@ -1,6 +1,5 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
-
 const isNumber = (n) => {
   if (!Number.isNaN(parseFloat(n)) && !Number.isNaN(n - 0)) {
     return parseInt(n, 10);
@@ -11,11 +10,11 @@ const iniFix = (obj) => {
   const entries = Object.entries(obj);
   return entries.reduce((acc, [key, value]) => {
     if (value instanceof Object) {
-      return { ...acc, [key]: iniFix(value) };
+      return {...acc, [key]: iniFix(value) };
     }
-    return { ...acc, [key]: isNumber(value) };
-  }, {});
-};
+    return {...acc, [key]: isNumber(value)};
+  }, {})
+}
 
 export default (data, extName) => {
   switch (extName) {
@@ -24,8 +23,6 @@ export default (data, extName) => {
     case '.yaml':
       return yaml.load(data);
     case '.ini':
-      console.log('ini', iniFix(ini.parse(data)));
-      console.log('ini Before', ini.parse(data));
       return iniFix(ini.parse(data));
     default:
       throw new Error(`Unknown file type: '${extName}'!`);
