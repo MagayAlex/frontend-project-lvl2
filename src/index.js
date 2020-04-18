38af8c36d0332fede9119fee5ae979b267e8eb59
Alex Magay <magayalex@gmail.com>
2020-04-18 20:57:45 +0300

    rename func data to readData

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import parse from './parse.js';
 import buildAst from './buildAst.js';
 import format from './format';
 
-const data = (pathToFile) => fs.readFileSync(path.resolve(process.cwd(), pathToFile), 'utf8');
+const readData = (pathToFile) => fs.readFileSync(path.resolve(process.cwd(), pathToFile), 'utf8');
 const genDiff = (pathToFile1, pathToFile2, type = 'json') => {
-  const parsedFile1 = parse(data(pathToFile1), path.extname(pathToFile1));
-  const parsedFile2 = parse(data(pathToFile2), path.extname(pathToFile2));
+  const parsedFile1 = parse(readData(pathToFile1), path.extname(pathToFile1));
+  const parsedFile2 = parse(readData(pathToFile2), path.extname(pathToFile2));
   const astTree = buildAst(parsedFile1, parsedFile2);
   return format(astTree, type);
 };
