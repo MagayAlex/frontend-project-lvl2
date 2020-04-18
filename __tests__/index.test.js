89a499d349d170070fb60171397a76a7c323d776
Alex Magay <magayalex@gmail.com>
2020-04-18 23:43:37 +0300

    create const result for each type

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,10 +5,14 @@ import genDiff from '../src/index.js';
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 const arr = ['json', 'yaml', 'ini'];
+const resultTree = readFile('resultTree.txt');
+const resultPlain = readFile('resultPlain.txt');
+const resultJson = readFile('resultJson.txt');
+
 test.each(arr)('test %%%', (etxtention) => {
   const fileBefore = getFixturePath(`beforeTree.${etxtention}`);
   const fileAfter = getFixturePath(`afterTree.${etxtention}`);
-  expect(genDiff(fileBefore, fileAfter, 'tree')).toEqual(readFile('resultTree.txt'));
-  expect(genDiff(fileBefore, fileAfter, 'plain')).toEqual(readFile('resultPlain.txt'));
-  expect(genDiff(fileBefore, fileAfter, 'json')).toEqual(readFile('resultJson.txt'));
+  expect(genDiff(fileBefore, fileAfter, 'tree')).toEqual(resultTree);
+  expect(genDiff(fileBefore, fileAfter, 'plain')).toEqual(resultPlain);
+  expect(genDiff(fileBefore, fileAfter, 'json')).toEqual(resultJson);
 });
