183c431c872d536d68d165e0ed06378c67c61680
Alex Magay <magayalex@gmail.com>
2020-04-20 20:02:51 +0300

    replace reduce to map function

--- src/buildAst.js
+++ src/buildAst.js
@@ -1,24 +1,24 @@
-import { has, uniq } from 'lodash';
+import { has, uniq, isObject } from 'lodash';
 
 const buildAst = (data1, data2) => {
   const uniqueKeys = uniq([...Object.keys(data1), ...Object.keys(data2)]).sort();
-  return uniqueKeys.reduce((acc, key) => {
+  return uniqueKeys.map((key) => {
     if (!has(data2, key)) {
-      return [...acc, { status: 'deleted', key, value: data1[key] }];
+      return [{ status: 'deleted', key, value: data1[key] }];
     }
     if (!has(data1, key)) {
-      return [...acc, { status: 'added', key, value: data2[key] }];
+      return [{ status: 'added', key, value: data2[key] }];
     }
-    if (typeof data1[key] === 'object' && typeof data2[key] === 'object') {
-      return [...acc, { status: 'nested', key, children: buildAst(data1[key], data2[key]) }];
+    if (isObject(data1[key]) && isObject(data2[key])) {
+      return [{ status: 'nested', key, children: buildAst(data1[key], data2[key]) }];
     }
     if (data1[key] === data2[key]) {
-      return [...acc, { status: 'nested', key, value: data1[key] }];
+      return [{ status: 'unchanged', key, value: data1[key] }];
     }
 
-    return [...acc, {
+    return [{
       status: 'changed', key, newValue: data2[key], oldValue: data1[key],
     }];
-  }, []);
+  }).flat(Infinity);
 };
 export default buildAst;
