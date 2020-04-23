9986fa2a0bd895c5634cabb758685821c7ee9230
Alex Magay <magayalex@gmail.com>
2020-04-23 19:50:23 +0300

    refactoring stringify func

--- src/format/buildTreeView.js
+++ src/format/buildTreeView.js
@@ -10,48 +10,24 @@ const buildSymbol = (status) => {
   }
   return tab;
 };
-const stringify = (nodeValue) => `${Object.entries(nodeValue).map(([key, value]) => `${key}: ${value}`).join()}`;
-const render = (node, depth = 0) => {
-  const makeTab = (count) => `${tab.repeat(count)}`;
-  if (node.status === 'changed') {
-    if (isObject(node.newValue) && isObject(node.oldValue)) {
-      return `\n${makeTab(depth)}${buildSymbol('deleted')}${node.key}: {\n${makeTab(depth + 2)}${stringify(node.oldValue)}\n${makeTab(depth + 1)}
-      \n${tab.repeat(depth)}${buildSymbol('added')}${node.key}: {\n${makeTab(depth + 2)}${stringify(node.newValue)}{\n${makeTab(depth + 1)}`;
-    }
-    if (typeof node.newValue === 'object') {
-      return `\n${makeTab(depth)}${buildSymbol('deleted')}${node.key}: ${node.oldValue}\n${makeTab(depth)}${buildSymbol('added')}${node.key}: {\n${makeTab(depth + 2)}${stringify(node.newValue)}\n${makeTab(depth + 1)}}`;
-    }
-    if (typeof node.oldValue === 'object') {
-      return `\n${makeTab(depth)}${buildSymbol('deleted')}${node.key}: {\n${makeTab(depth + 2)}${stringify(node.oldValue)}\n${makeTab(depth + 1)}}\n${makeTab(depth)}${buildSymbol('added')}${node.key}: ${node.newValue}`;
-    }
-    return `\n${makeTab(depth)}${buildSymbol('deleted')}${node.key}: ${node.oldValue}\n${makeTab(depth)}${buildSymbol('added')}${node.key}: ${node.newValue}`;
+const makeTab = (count) => `${tab.repeat(count)}`;
+const stringify = (nodeValue, tabSize) => {
+  if (isObject(nodeValue)) {
+    return `{\n${makeTab(tabSize + 2)}${Object.entries(nodeValue).map(([key, value]) => `${key}: ${value}`).join()}\n${makeTab(tabSize + 1)}}`;
   }
+  return nodeValue;
+};
+const render = (node, depth = 0) => {
   if (node.status === 'nested') {
     return `\n${makeTab(depth)}${buildSymbol(node.status)}${node.key}: {${node.children.map((child) => render(child, depth + 1)).join('')}\n${makeTab(depth + 1)}}`;
   }
-  if (node.status === 'unchanged') {
-    if (typeof node.value === 'object') {
-      return `\n${makeTab(depth)}${buildSymbol(node.status)}${node.key}: \n${makeTab(depth + 2)}${stringify(node.value)}\n${makeTab(depth + 1)}}`;
-    }
-    return `\n${makeTab(depth)}${buildSymbol(node.status)}${node.key}: ${(node.value)}`;
+  if (node.status === 'changed') {
+    return `\n${makeTab(depth)}${buildSymbol('deleted')}${node.key}: ${stringify(node.oldValue, depth)}\n${makeTab(depth)}${buildSymbol('added')}${node.key}: ${stringify(node.newValue, depth)}`;
   }
-  if (node.status === 'added') {
-    if (typeof node.value === 'object') {
-      return `\n${makeTab(depth)}${buildSymbol(node.status)}${node.key}: {\n${makeTab(depth + 2)}${stringify(node.value)}\n${makeTab(depth + 1)}}`;
-    }
-    return `\n${makeTab(depth)}${buildSymbol(node.status)}${node.key}: ${(node.value)}`;
-  }
-  if (node.status === 'deleted') {
-    if (isObject(node.value)) {
-      return `\n${makeTab(depth)}${buildSymbol(node.status)}${node.key}: {\n${makeTab(depth + 2)}${stringify(node.value)}\n${makeTab(depth + 1)}}`;
-    }
-    return `\n${makeTab(depth)}${buildSymbol(node.status)}${node.key}: ${(node.value)}`;
-  }
-  return true;
+  return `\n${makeTab(depth)}${buildSymbol(node.status)}${node.key}: ${stringify(node.value, depth)}`;
 };
 const buildTreeView = (data) => {
   const x = data.reduce((acc, node) => `${acc}${render(node, 0)}`, '');
   return `{${x}\n}`;
 };
-
 export default buildTreeView;
