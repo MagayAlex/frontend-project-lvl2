71b7ba348a99b6eb740bf25e98e56f2b6804ec97
Alex Magay <magayalex@gmail.com>
2020-04-27 11:57:32 +0300

    delete "flatten" and uniq => union

--- src/buildAst.js
+++ src/buildAst.js
@@ -1,25 +1,25 @@
 import {
-  has, uniq, isObject, flatten,
+  has, union, isObject,
 } from 'lodash';
 
 const buildAst = (data1, data2) => {
-  const uniqueKeys = uniq([...Object.keys(data1), ...Object.keys(data2)]).sort();
-  return flatten(uniqueKeys.map((key) => {
+  const keys = union(Object.keys(data1), Object.keys(data2)).sort();
+  return keys.map((key) => {
     if (!has(data2, key)) {
-      return [{ status: 'deleted', key, value: data1[key] }];
+      return { status: 'deleted', key, value: data1[key] };
     }
     if (!has(data1, key)) {
-      return [{ status: 'added', key, value: data2[key] }];
+      return { status: 'added', key, value: data2[key] };
     }
     if (isObject(data1[key]) && isObject(data2[key])) {
-      return [{ status: 'nested', key, children: buildAst(data1[key], data2[key]) }];
+      return { status: 'nested', key, children: buildAst(data1[key], data2[key]) };
     }
     if (data1[key] === data2[key]) {
-      return [{ status: 'unchanged', key, value: data1[key] }];
+      return { status: 'unchanged', key, value: data1[key] };
     }
-    return [{
+    return {
       status: 'changed', key, newValue: data2[key], oldValue: data1[key],
-    }];
-  }));
+    };
+  });
 };
 export default buildAst;
