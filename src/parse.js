5e27869ee746aa609f09f1d02760b113d52a317d
Alex Magay <magayalex@gmail.com>
2020-04-27 12:12:32 +0300

    add object parsers

--- src/parse.js
+++ src/parse.js
@@ -1,31 +1,29 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-const toNumber = (n) => {
-  if (!Number.isNaN(parseFloat(n))) {
-    return parseFloat(n);
+const toNumber = (value) => {
+  if (!Number.isNaN(parseFloat(value))) {
+    return parseFloat(value);
   }
-  return n;
+  return value;
 };
-const iniFixNumbers = (obj) => {
+const numberifyValues = (obj) => {
   const entries = Object.entries(obj);
   return entries.reduce((acc, [key, value]) => {
     if (value instanceof Object) {
-      return { ...acc, [key]: iniFixNumbers(value) };
+      return { ...acc, [key]: numberifyValues(value) };
     }
     return { ...acc, [key]: toNumber(value) };
   }, {});
 };
-
+const parsers = {
+  json: (data) => JSON.parse(data),
+  yaml: (data) => yaml.load(data),
+  ini: (data) => numberifyValues(ini.parse(data)),
+};
 export default (data, type) => {
-  switch (type) {
-    case 'json':
-      return JSON.parse(data);
-    case 'yaml':
-      return yaml.load(data);
-    case 'ini':
-      return iniFixNumbers(ini.parse(data));
-    default:
-      throw new Error(`Unknown file type: '${type}'!`);
+  if (!parsers[type]) {
+    throw new Error(`Unknown file type: '${type}'!`);
   }
+  return parsers[type](data);
 };
