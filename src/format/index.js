bfbd34cabbe77336d85777a55731e7fbb3f35c0c
Alex Magay <magayalex@gmail.com>
2020-04-27 12:37:33 +0300

    add mapping

--- src/format/index.js
+++ src/format/index.js
@@ -2,15 +2,14 @@ import buildTreeView from './tree.js';
 import buildPlainView from './plain.js';
 import buildJsonView from './json.js';
 
+const mapping = {
+  tree: (data) => buildTreeView(data),
+  plain: (data) => buildPlainView(data),
+  json: (data) => buildJsonView(data),
+};
 export default (data, type) => {
-  if (type === 'tree') {
-    return buildTreeView(data);
+  if (!mapping[type]) {
+    throw new Error(`Unknown format type: '${type}'!`);
   }
-  if (type === 'plain') {
-    return buildPlainView(data);
-  }
-  if (type === 'json') {
-    return buildJsonView(data);
-  }
-  return 'error: unsupported type';
+  return mapping[type](data);
 };
