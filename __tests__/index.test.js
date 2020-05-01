a88d04cf43b07763c7a6c28c34125fec85927e75
Alex Magay <magayalex@gmail.com>
2020-05-01 21:30:02 +0300

    add beforeAll func

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,10 +5,14 @@ import genDiff from '../src/index.js';
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 const arr = ['json', 'yaml', 'ini'];
-const resultTree = readFile('resultTree.txt');
-const resultPlain = readFile('resultPlain.txt');
-const resultJson = readFile('resultJson.txt');
-
+let resultTree;
+let resultPlain;
+let resultJson;
+beforeAll(() => {
+  resultTree = readFile('resultTree.txt');
+  resultPlain = readFile('resultPlain.txt');
+  resultJson = readFile('resultJson.txt');
+});
 test.each(arr)('test %%%', (etxtention) => {
   const fileBefore = getFixturePath(`beforeTree.${etxtention}`);
   const fileAfter = getFixturePath(`afterTree.${etxtention}`);
