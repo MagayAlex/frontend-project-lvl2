dcc190a1c5a8c4c139ed53e326650934c397538e
Alex Magay <magayalex@gmail.com>
2020-05-02 13:18:27 +0300

    fix stringify

--- src/format/tree.js
+++ src/format/tree.js
@@ -4,8 +4,8 @@ const tab = '    ';
 const makeTab = (count) => tab.repeat(count);
 const stringify = (nodeValue, depth) => {
   if (isObject(nodeValue)) {
-    const entries = Object.entries(nodeValue).map(([key, value]) => `${key}: ${stringify(value, depth + 1)}`).join(`\n${makeTab(depth + 2)}`);
-    return `{\n${makeTab(depth + 2)}${entries}\n${makeTab(depth + 1)}}`;
+    const entries = Object.entries(nodeValue).map(([key, value]) => `${makeTab(depth + 2)}${key}: ${stringify(value, depth + 1)}`).join(`\n${makeTab(depth + 2)}`);
+    return `{\n${entries}\n${makeTab(depth + 1)}}`;
   }
   return nodeValue;
 };
