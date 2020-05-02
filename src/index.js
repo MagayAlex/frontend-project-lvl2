31c4967016d912af4a4e308824163462ac730091
Alex Magay <magayalex@gmail.com>
2020-05-02 14:51:53 +0300

    fix naming

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
 const getFormat = (filepath) => path.extname(filepath).slice(1);
 const readFile = (filepath) => fs.readFileSync(filepath, 'utf8');
 const getData = (filepath) => parse(readFile(getFullPath(filepath)), getFormat(filepath));
-const genDiff = (pathToFile1, pathToFile2, formatName = 'json') => {
-  const parsedFile1 = getData(pathToFile1);
-  const parsedFile2 = getData(pathToFile2);
-  const astTree = buildAst(parsedFile1, parsedFile2);
+const genDiff = (filepath1, filepath2, formatName = 'json') => {
+  const data1 = getData(filepath1);
+  const data2 = getData(filepath2);
+  const astTree = buildAst(data1, data2);
   return format(astTree, formatName);
 };
 export default genDiff;
